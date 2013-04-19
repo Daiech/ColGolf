7568d7022bbcadecbd8ca96747075598e8d39dcc
Mauricio <mauricioaizaga@gmail.com>
2013-04-19 14:17:13 -0500

    login required

--- ColGolf/urls.py
+++ ColGolf/urls.py
@@ -8,11 +8,14 @@ urlpatterns = patterns(
     '',
     # Examples:
     url(r'^$', 'website.views.home', name='home'),
+    url(r'^accounts/profile/$', 'website.views.home', name='home'),
     # url(r'^ColGolf/', include('ColGolf.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
